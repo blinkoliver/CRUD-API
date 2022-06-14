8506cd2b72e062e0f873e2dc94dc4aa2453e1d2a
Anton Barouka <borovko28@gmail.com>
2022-06-15 01:37:56 +0300

    init: start simple crud task

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(8000);
