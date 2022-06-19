842db658ef0c7b7787ee7a799ba3150f5a148aa6
Anton Barouka <borovko28@gmail.com>
2022-06-19 21:17:07 +0300

    feat: put method

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,10 +1,40 @@
 import { users } from "./users";
+import { v4 as uuidv4 } from "uuid";
 interface user {
   id: string;
   username: string;
   age: number;
   hobbies: string[];
 }
-export const findOne = async (id: string) => {
-  return users.find((el: user) => el.id === id);
+
+export const findOne = (id: string) => {
+  const desired = users.find((el: user) => el.id === id);
+  if (desired) {
+    return desired;
+  } else {
+    return undefined;
+  }
+};
+export const writeOne = (data: any): void => {
+  const newUser = { ...data, id: uuidv4() };
+  users.push(newUser);
+  console.log(users);
+};
+
+export const updateOne = (id: string, data: any): any => {
+  const desired = findOne(id);
+  const indexInUsers = users.findIndex((el) => el.id === id);
+  if (desired) {
+    const updatedUser = {
+      ...desired,
+      username: data.username,
+      age: data.age,
+      hobbies: data.hobbies,
+    };
+    users.splice(indexInUsers, 1);
+    users.splice(indexInUsers, 0, updatedUser);
+    return updatedUser;
+  } else {
+    return undefined;
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
 import { createServer } from "http";
 import dotenv from "dotenv";
-import { parseURL } from "./utils";
+import { parseURL, getReqData } from "./utils";
 import { users } from "./users";
-import { findOne } from "./controllers";
+import { findOne, updateOne, writeOne } from "./controllers";
+import { validate as isValidUUID } from "uuid";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const getHandler = async (req: any) => {
-  const parseString = parseURL(req);
-  if (req === parseString) {
+  const parseUserId = parseURL(req);
+  if (req === parseUserId) {
     return users;
+  } else if (isValidUUID(parseUserId)) {
+    const resultFromFind = findOne(parseUserId);
+    return resultFromFind;
   } else {
-    return await findOne(parseString);
+    return "Invalid id";
   }
 };
-const postHandler = (req: any) => {
-  console.log("post", req);
+const postHandler = async (req: any) => {
+  return await getReqData(req);
 };
-const putHandler = (req: any) => {
-  console.log("put", req);
+const putHandler = async (req: any) => {
+  const parseUserId = parseURL(req.url);
+  const parseData = await getReqData(req);
+  if (isValidUUID(parseUserId)) {
+    const resultFromUpdate = updateOne(parseUserId, parseData);
+    if (resultFromUpdate) {
+      return resultFromUpdate;
+    } else {
+      return undefined;
+    }
+  } else {
+    return "Invalid id";
+  }
 };
 const deleteHandler = (req: any) => {
   console.log("delete", req);
@@ -28,22 +43,51 @@ const deleteHandler = (req: any) => {
 const server = createServer(async (req, res) => {
   switch (req.method) {
     case "GET":
-      const result = await getHandler(req.url);
-      if (result) {
+      const resultFromGet = await getHandler(req.url);
+      if (resultFromGet && typeof resultFromGet !== "string") {
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ data: result }));
+        res.end(JSON.stringify({ data: resultFromGet }));
+      } else if (resultFromGet) {
+        res.writeHead(400, "invalid ID", {
+          "Content-Type": "application/json",
+        });
+        res.end("error, invalid ID");
       } else {
         res.writeHead(404, "doesn't exist", {
           "Content-Type": "application/json",
         });
-        res.end("error");
+        res.end("error, doesn't exist");
       }
       break;
     case "POST":
-      postHandler(req.url);
+      const resultFromPost = await postHandler(req);
+      if (typeof resultFromPost !== "string") {
+        writeOne(resultFromPost);
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ data: resultFromPost }));
+      } else {
+        res.writeHead(400, resultFromPost, {
+          "Content-Type": "application/json",
+        });
+        res.end(`error, ${resultFromPost}`);
+      }
       break;
     case "PUT":
-      putHandler(req.url);
+      const resultFromPut = await putHandler(req);
+      if (resultFromPut && typeof resultFromPut !== "string") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ data: resultFromPut }));
+      } else if (resultFromPut) {
+        res.writeHead(400, "invalid ID", {
+          "Content-Type": "application/json",
+        });
+        res.end("error, invalid ID");
+      } else {
+        res.writeHead(404, "doesn't exist", {
+          "Content-Type": "application/json",
+        });
+        res.end("error, doesn't exist");
+      }
       break;
     case "DELETE":
       deleteHandler(req.url);
